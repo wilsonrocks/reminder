7414b0119b9506f70b99d224c2ae3f78b5fd2f29
James Wilson <oh.that.wilson@googlemail.com>
2017-05-03 15:11:00 +0100

    Initial setting up of anniversary model.

--- reminder.py
+++ reminder.py
@@ -2,3 +2,18 @@ import argparse
 
 import peewee
 
+import secrets
+
+dbfile = "reminder.db"
+db = peewee.SqliteDatabase(dbfile)
+
+class ReminderModel(peewee.Model):
+    class Meta:
+        database = db
+
+class Anniversary(ReminderModel):
+    date = peewee.DateField()
+
+    name = peewee.CharField()
+
+db.create_tables([Anniversary],safe=True)
